592c130fad6e4001a3e327fbc0c298d864986e03
Ubuntu <ubuntu@ip-10-0-3-253.eu-west-2.compute.internal>
2025-01-21 18:21:18 +0000

    Initial commit

---
+++ list-users.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#
+######################
+#Author : Chaitanya
+#Date 21 Jan 2025
+#Purpose : Shell Script to list the users having access to a repo
+####################
+#
+#GitHub API url
+APIURL = "https://api.github.com"
+
+#Github user name and personal access token
+
+USERNAME = $user
+TOKEN = $token
+
+#Organisation and repo information
+ORGNAME = $1
+REPONAME = $2
+
+#Function to make a get request to the github API
+function github_get_api{
+	local endpoint = "$1"
+	local url = "${APIURL}/${endpoint}"
+	#send a GET request to API with authentication
+	
+	curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+#Function to list users of the repository
+function list_users_of_repo{
+	local endpoint = "repos/${ORGNAME}/${REPONAME}/collaborators"
+
+	#fetch list of collaborators of the repo
+	collaborators = "$(github_get_api "$endpoint" | jq -r '.[] | select(.permissions.admin == true) | .login')"
+
+	#display the lust of collaborators with read access
+	if [[-z "$collaborators" ]]; then
+		echo "No users with read access for ${ORGNAME}/${REPONAME}"
+	else
+		echo "users list with read access to ${ORGNAME}/${REPONAME}"
+		echo "$collaborators"
+
+	fi
+#Main script
+echo "Listening to repo for users with read access"
+list_users_of_repo
