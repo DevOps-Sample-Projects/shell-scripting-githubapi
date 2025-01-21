a37f577d248f8984ec6be58583c19a8b5b16349b
Ubuntu <ubuntu@ip-10-0-3-253.eu-west-2.compute.internal>
2025-01-21 18:42:39 +0000

    updated the code

--- list-users.sh
+++ list-users.sh
@@ -7,41 +7,42 @@
 ####################
 #
 #GitHub API url
-APIURL = "https://api.github.com"
+APIURL="https://api.github.com"
 
 #Github user name and personal access token
 
-USERNAME = $user
-TOKEN = $token
+USERNAME=$user
+TOKEN=$token
 
 #Organisation and repo information
-ORGNAME = $1
-REPONAME = $2
+ORGNAME=$1
+REPONAME=$2
 
 #Function to make a get request to the github API
-function github_get_api{
-	local endpoint = "$1"
-	local url = "${APIURL}/${endpoint}"
+function github_get_api {
+	local endpoint="$1"
+	local url="${APIURL}/${endpoint}"
 	#send a GET request to API with authentication
 	
 	curl -s -u "${USERNAME}:${TOKEN}" "$url"
 }
 
 #Function to list users of the repository
-function list_users_of_repo{
-	local endpoint = "repos/${ORGNAME}/${REPONAME}/collaborators"
+function list_users_of_repo {
+	local endpoint="repos/${ORGNAME}/${REPONAME}/collaborators"
 
 	#fetch list of collaborators of the repo
-	collaborators = "$(github_get_api "$endpoint" | jq -r '.[] | select(.permissions.admin == true) | .login')"
+	collaborators="$(github_get_api "$endpoint")"
 
 	#display the lust of collaborators with read access
-	if [[-z "$collaborators" ]]; then
+	if [[ -z "$collaborators" ]]; then
 		echo "No users with read access for ${ORGNAME}/${REPONAME}"
 	else
 		echo "users list with read access to ${ORGNAME}/${REPONAME}"
 		echo "$collaborators"
 
 	fi
+}
 #Main script
 echo "Listening to repo for users with read access"
 list_users_of_repo
